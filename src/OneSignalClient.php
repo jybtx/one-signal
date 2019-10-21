9017946358537a9f0c956c8c3bb79f1322480686
jybtx <2574034626@qq.com>
2019-10-21 11:26:23 +0800

    添加给标签用户发送消息和查看通知及csv导出

--- src/OneSignalClient.php
+++ src/OneSignalClient.php
@@ -28,19 +28,19 @@ class OneSignalClient
      * @param  [type] $device_model [推荐的设备品牌和型号。例：iPhone5,1]
      * @return [type]               [description]
      */
-    public function registerPlayerId($identifier,$device_type,$device_os='',$device_model='')
+    public function registerPlayerId($identifier,$device_type,$tags=NULL,$version='1.0',$device_os='',$device_model='')
     {
         $fields = array( 
 			'app_id'       => $this->appId, 
 			'identifier'   => $identifier, 
 			'language'     => config('app.locale'), 
 			'timezone'     => config('app.timezone'), 
-			'game_version' => "1.0", 
+			'game_version' => $version, 
 			'device_os'    => $device_os, 
 			'device_type'  => $device_type, 
 			'device_model' => $device_model, 
-			'tags'         => array("foo" => "app") 
         );
+        if ( isset($tags) ) $fields['tags'] = $tags;
 
         return self::getMethod($fields, self::getUrlMethod( self::ENDPOINT_PLAYERS ) );      
     }
@@ -50,7 +50,7 @@ class OneSignalClient
      * @param  [string] $txt   [推送消息主体内容]
      * @return [array]         [推送消息返回数据]
      */
-    public function sendMessageAllUsers($title,$txt,$time=null,$data = array())
+    public function sendMessageAllUsers($title,$txt,$time=NULL,$data = array(),$url = NULL, $buttons = NULL)
     {
         $fields = array(
 			'app_id'            => $this->appId,
@@ -59,7 +59,9 @@ class OneSignalClient
 			'headings'          => array('en'=>$title),
 			'contents'          => array('en'=>$txt),
         );
-        if ( !empty( $data ) ) $fields['data'] = $data;
+        if ( isset( $data ) ) $fields['data'] = $data;
+        if ( isset( $url ) ) $fields['url'] = $url;
+        if ( isset( $buttons ) ) $fields['buttons'] = $buttons;
         return self::getMethod($fields, self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) );
     }
     /**
@@ -72,16 +74,79 @@ class OneSignalClient
      * @param  array      $data  [自定义字段]
      * @return [type]            [description]
      */
-    public function sendMessageSomeUser($title,$txt,$users,$data = array())
+    public function sendMessageSomeUser($title,$txt,$users,$data = array(),$url = NULL,$buttons = NULL)
     {
         $fields = array(
 			'app_id'             => $this->appId,
-			'include_player_ids' => is_array( $users )?:[$users],
+			'include_player_ids' => is_array( $users )?:array($users),
 			'headings'           => array('en'=>$title),
 			'contents'           => array('en'=>$txt),
         );
-        if ( !empty( $data ) ) $fields['data'] = $data;
+        if ( isset( $data ) ) $fields['data'] = $data;
+        if ( isset( $url ) ) $fields['url'] = $url;
+        if ( isset( $buttons ) ) $fields['buttons'] = $buttons;
         return self::getMethod($fields, self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) );
+    }
+    /**
+     * 给标签用户发送消息
+     * @author jybtx
+     * @date   2019-10-21
+     * @param  [type]     $title    [description]
+     * @param  [type]     $txt      [description]
+     * @param  [type]     $tags     [description]
+     * @param  [type]     $url      [description]
+     * @param  [type]     $data     [description]
+     * @param  [type]     $buttons  [description]
+     * @param  [type]     $subtitle [description]
+     * @return [type]               [description]
+     */
+    public function sendMessageUsingTags($title,$txt, $tags, $url = NULL, $data = NULL, $buttons = NULL, $subtitle = NULL)
+    {
+        $fields = array(
+            'app_id'   => $this->appId,
+            'filters'  => $tags,
+            'headings' => array('en'=>$title),
+            'contents' => array('en'=>$txt),
+        );
+        $fields['url']      = $url??'';
+        $fields['data']     = $data??'';
+        $fields['buttons']  = $buttons??'';
+        $fields['subtitle'] = $subtitle??'';
+        return self::getMethod($fields, self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) );
+    }
+    /**
+     * 查看通知
+     * @author jybtx
+     * @date   2019-10-21
+     * @param  [type]     $limit  [description]
+     * @param  [type]     $offset [description]
+     * @param  [type]     $kind   [description]
+     * @return [type]             [description]
+     */
+    public function getNotifications($limit = NULL, $offset = NULL, $kind = 1 )
+    {
+        $fields = array(
+            'app_id'   => $this->appId,
+        );
+        $filters['limit']   = $limit??'';
+        $filters['offset '] = $offset??'';
+        $filters['offset '] = $offset??'';
+        $filters['kind ']   = $kind??'';
+        return self::getMethod($fields, self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) );
+    }
+    /**
+     * 生成所有当前用户数据的压缩CSV导出
+     * @author jybtx
+     * @date   2019-10-21
+     * @return [type]     [description]
+     */
+    public function getAllUserToExportCsv()
+    {
+        $fields = array(
+            'app_id'   => $this->appId,
+        );
+        $url = self::getUrlMethod( self::ENDPOINT_PLAYERS ).'/csv_export';
+        return self::getMethod($fields, $url );
     }
     /**
      * 取消通知---已发送的不能取消
@@ -90,11 +155,8 @@ class OneSignalClient
      */
     public function revokeMessage($notifId)
     {
-        $fields = array(
-            'app_id' => $this->appId
-        );
-        $url = self::getUrlMethod( self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) ) . $notifId;
-        return self::getMethod($fields,$url,'DELETE');
+        $url = self::getUrlMethod( self::ENDPOINT_NOTIFICATIONS ) .'/'. $notifId."?app_id=".$this->appId;
+        return self::getMethod('',$url,'DELETE');
     }
     /**
      * 请求方法
